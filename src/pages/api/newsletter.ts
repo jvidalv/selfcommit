ba4697cfd71654e9f6767d01f8c36aa676dfbcd6
jvidalv <josepvidalvidal@gmail.com>
2022-11-14 18:47:05 +0100

    Mobiles fixes for build page

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,9 +1,5 @@
-import { Client } from '@notionhq/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { addRowInNotion } from 'utils/notion'
-
-const notion = new Client({ auth: process.env.NOTION_KEY })
-const databaseId = process.env.NOTION_DATABASE_ID!
 
 export default async function handler(
   req: NextApiRequest,
